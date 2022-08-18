1973d06a14b02ca3c5c51cf4925b45131ccd0f66
Folgue02 <davidfolru@gmail.com>
2022-08-18 14:54:14 +0200

    Last touches

---
+++ shell_implementations/bash_implementation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# To make this script usable just copy the function below
+# and paste it on your ~/.bashrc
+
+goto() {
+    EXCHANGE_PATH="/tmp/gotochoice"
+    ## The first one is useful for testing, the second one is the one 
+    ## supposed to be used in production.
+    #GOTO="target/debug/goto"
+    GOTO=$(which goto)
+
+    # Make sure an old path its not used.
+    if [ -f $EXCHANGE_PATH ]; then 
+        rm $EXCHANGE_PATH
+    fi
+
+    if [ -f $GOTO ]; then
+        $GOTO
+
+        if [ -f $EXCHANGE_PATH ]; then 
+            TARGET_PATH=$(cat $EXCHANGE_PATH)
+            echo "Setting location to '$TARGET_PATH'..."
+            cd $TARGET_PATH
+            rm $EXCHANGE_PATH
+        else
+            # Nothing was selected in the goto process
+            cd $(cat $EXCHANGE_PATH)
+            echo "Nothing was selected in goto."
+        fi
+    else
+        echo "The goto path executable doesn't exist."
+    fi
+}
+
+goto
---
+++ shell_implementations/powershell_implementation.ps1
@@ -0,0 +1,38 @@
+#!/opt/microsoft/powershell/7/pwsh
+
+# To make this script usable just copy the function below and paste
+# it in your $profile file (`~/.config/powershell/Microsoft.Powershell_profile.ps1`)
+
+function goto () {
+    ## The first one is useful for testing, the second one is the one 
+    ## supposed to be used in production
+
+    #$goto = (Join-Path "target" "debug" "goto")
+    $goto = which "goto"
+
+    $exchange_path = Join-Path "/" "tmp" "gotochoice"
+
+
+    if (Test-Path $goto) {
+
+        # Make sure an old path its not used.
+        if (Test-Path $exchange_path) {
+            Remove-Item $exchange_path
+        }
+
+        Start-Process $goto -Wait
+        if (Test-Path $exchange_path) {
+            $target_path = (Get-Content $exchange_path).Trim()
+            Write-Host "Setting location to '$target_path'..."
+            Set-Location $target_path
+            Remove-Item $exchange_path
+        } else {
+            # Nothing was selected in the goto process.
+            Write-Host "Nothing was selected in goto."
+        }
+    } else {
+        Write-Host "The goto path executable doesn't exist." -ForegroundColor Red    
+    }
+}
+
+goto
